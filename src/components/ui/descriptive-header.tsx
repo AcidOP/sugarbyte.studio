656452723fe619bbdde2a9d8c03610cf4732e82b
Zeeshan <zeeshanalivr46@gmail.com>
2026-02-07 10:20:05 +0530

    good

--- src/components/ui/descriptive-header.tsx
+++ src/components/ui/descriptive-header.tsx
@@ -1,3 +1,5 @@
+import HalfLayout from '../layouts/half-layout';
+
 import { cn } from '@/lib/utils';
 
 import type { ReactNode } from 'react';
@@ -11,15 +13,27 @@ interface IProps {
   className?: string;
 }
 
+const Count = ({ value }: { value: number }) => {
+  return (
+    <p className='font-primary text-4xl text-neutral-500'>
+      (<span className='px-1 font-black text-neutral-700'>{value}</span>)
+    </p>
+  );
+};
+
 const DescriptiveHeader = ({ count, heading, description, className }: IProps) => {
   return (
-    <section className={cn('flex items-center justify-between py-48', className)}>
-      <span className='text-3xl font-medium text-neutral-400'>
-        (<span className='text-5xl text-neutral-700'>{count}</span>)
-      </span>
-      <h2 className='font-primary text-7xl font-black'>{heading}</h2>
-      <p className='max-w-sm text-xl font-medium text-neutral-700'>{description}</p>
-    </section>
+    <HalfLayout className={cn('py-48', className)}>
+      <HalfLayout.LeftChild>
+        <h2 className='font-primary text-7xl font-black'>{heading}</h2>
+        <Count value={count} />
+      </HalfLayout.LeftChild>
+      <HalfLayout.RightChild>
+        <p className='max-w-lg text-xl font-medium text-neutral-700'>
+          {description}
+        </p>
+      </HalfLayout.RightChild>
+    </HalfLayout>
   );
 };
 
--- src/modules/works/works.tsx
+++ src/modules/works/works.tsx
@@ -1,26 +1,12 @@
 import Container from '@/components/layouts/container';
 
-import DescriptiveHeader from '@/components/ui/descriptive-header';
-
-const Box = () => <div className='aspect-video w-full bg-neutral-400 shadow-2xl' />;
-
 const Works = () => {
   return (
-    <Container className='my-10 min-h-screen'>
-      <DescriptiveHeader
-        count={7}
-        heading='Selected Works.'
-        description="We've helped businesses across industries achieve their goals. Here
-          are some of our recent projects."
-      />
-
-      <section className='grid grid-cols-1 gap-5 lg:grid-cols-2'>
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-      </section>
-    </Container>
+    <section className='bg-lime-400'>
+      <Container className='font-primary my-20 grid min-h-screen place-content-center text-center text-[10rem] leading-36 font-black'>
+        SELECTED <br /> WORKS <br /> (2024-2026)
+      </Container>
+    </section>
   );
 };
 
